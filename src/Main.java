0e0b006218f501045df67be13d720948c947976b
Josue Mullo <josue.mullo@udla.edu.ec>
2025-02-22 22:33:35 -0500

    Optimizando if de verifyWinner()

--- src/Main.java
+++ src/Main.java
@@ -109,11 +109,8 @@ public class Main {
                 return true;
             }
         }
-        if ((board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) ||
-                (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer)) {
-            return true;
-        }
-        return false;
+        return (board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) ||
+                (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer);
     }
 
     /**
